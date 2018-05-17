1390e7d4b18da097e9d536bb5845cc025c52d082
Sergey Karabuta <siriusitsastar@gmail.com>
2018-05-18 01:22:59 +0300

    Задача 1.7

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Rhombus
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            bool b = true;
+            float f = 2.6F;
+            char c = 'r';
+            double d = 600D;
+            long l = 23492034207;
+            byte bt = 123;
+            String s = "May the force will be with you, always...";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(f);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(l);
+            Console.WriteLine(bt);
+            Console.WriteLine(s);
+
+        }
+    }
+}

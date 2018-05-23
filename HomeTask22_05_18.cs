341e1b0f5915e33197a3cc7703dad96da57445a6
Sergey Karabuta <siriusitsastar@gmail.com>
2018-05-23 23:00:26 +0300

    Home Work 22.05.18

---
+++ HomeTask22_05_18.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 234234;
+            //double b = 356.6545;
+            //double c = -678.6786;
+            //Console.WriteLine("{0,-10}{1,-10}{2,-10} ", a, b, c);
+            //Console.ReadLine();
+
+            //int a, b;
+            //int maxInt, minInt;
+            //a = int.Parse(Console.ReadLine());
+            //b = int.Parse(Console.ReadLine());
+
+            //minInt = (a + b - Math.Abs(a - b)) / 2;
+            //maxInt = (a + b + Math.Abs(a - b)) / 2;
+
+            //Console.WriteLine("Меньшее число  {0}",minInt);
+            //Console.WriteLine("Bigger one  {0}",maxInt);
+            //Console.ReadLine();
+
+            Char c = '*';                      
+            
+
+            Console.WriteLine("{0,10}" , c );
+            Console.Write("{0,5}{0,7}", c);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
